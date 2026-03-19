1836f8441da41b76a82fbbee79038a8a5e3cc3d4
intina47 <isaiahntina47@gmail.com>
2026-03-19 17:57:48 +0000

    Update v1.5.4 release checksums

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $ErrorActionPreference = "Stop"
 
 $version = "1.5.4"
 $url = "https://github.com/Intina47/jot/releases/download/v$version/jot_v$version_windows_amd64.zip"
-$checksum = "950e97e635c273bb47ab25592b817a74900159f826f80091bfab0e51f407fb6c"
+$checksum = "eb37037e0804592c24717595abd586e4b01abeecd585a664bc4582bd04ac8c14"
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Install-ChocolateyZipPackage -PackageName "jot" -Url $url -UnzipLocation $toolsDir -Checksum $checksum -ChecksumType "sha256"
--- packaging/homebrew/jot.rb
+++ packaging/homebrew/jot.rb
@@ -6,16 +6,16 @@ class Jot < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/Intina47/jot/releases/download/v1.5.4/jot_v1.5.4_darwin_arm64.tar.gz"
-      sha256 "ef73e597a467a213aae07051e006dabab15553cc36597296f77d1104c1208f2c"
+      sha256 "4e8a4d3fd02a0aa59dda7ba0efa9311eb52ba7a857676f67ac9ccf7594a2e9f2"
     else
       url "https://github.com/Intina47/jot/releases/download/v1.5.4/jot_v1.5.4_darwin_amd64.tar.gz"
-      sha256 "5271482f0cb217a8c5b2fbe5bc3ea6ffa2cced9d2fc3755e23c6a4f90f67fdc7"
+      sha256 "4b8de503b1e069952da407a3a1f4aefd90172de65c722f2fdcce89fb78a950f7"
     end
   end
 
   on_linux do
     url "https://github.com/Intina47/jot/releases/download/v1.5.4/jot_v1.5.4_linux_amd64.tar.gz"
-    sha256 "a968fe0c24a15e739a8b9d7ee979747b1ed9c55bbe02a2b260922b30713d9403"
+    sha256 "82e336fb96e7afd7d66f6f5277912ec20c1464d7c5eca01bc1d24b4ddcf46bf2"
   end
 
   def install
