2137e5a7b3b639ca31079565b20b73b164ae7f42
intina47 <isaiahntina47@gmail.com>
2026-04-06 03:30:31 +0100

    Prepare v1.7.2 release

--- packaging/chocolatey/tools/chocolateyinstall.ps1
+++ packaging/chocolatey/tools/chocolateyinstall.ps1
@@ -1,9 +1,9 @@
 $ErrorActionPreference = "Stop"
 [Net.ServicePointManager]::SecurityProtocol = [Net.ServicePointManager]::SecurityProtocol -bor 3072
 
-$version = "1.7.0"
+$version = "1.7.2"
 $url = "https://github.com/Intina47/jot/releases/download/v$version/jot_v$version_windows_amd64.zip"
-$checksum = "d837d8b41d5544aba459749c6a5e64089b7525472113eb99e0718a715ab67c99"
+$checksum = "2f314097ccf5ae692fffd1524dcb9da719cea7a7c771b891a0c55e4a6bed2dd7"
 
 $toolsDir = Split-Path -Parent $MyInvocation.MyCommand.Definition
 Install-ChocolateyZipPackage -PackageName "jot" -Url $url -UnzipLocation $toolsDir -Checksum $checksum -ChecksumType "sha256"
--- packaging/homebrew/jot-cli.rb
+++ packaging/homebrew/jot-cli.rb
@@ -1,21 +1,21 @@
 class JotCli < Formula
   desc "Terminal-first notebook and local document viewer"
   homepage "https://github.com/Intina47/jot"
-  version "1.7.0"
+  version "1.7.2"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/Intina47/jot/releases/download/v1.7.0/jot_v1.7.0_darwin_arm64.tar.gz"
-      sha256 "c5c78b9c2061d37c9b7b380e2d1b275cd48c5e4336d04c9dc95a4106736d468f"
+      url "https://github.com/Intina47/jot/releases/download/v1.7.2/jot_v1.7.2_darwin_arm64.tar.gz"
+      sha256 "bef70d22f14511715ec2a1821d376b0b95a82a7ab031c72d2b589a0a554b1914"
     else
-      url "https://github.com/Intina47/jot/releases/download/v1.7.0/jot_v1.7.0_darwin_amd64.tar.gz"
-      sha256 "c5602518ac977a893988f9dc5e7339c5ac20d0e293a0b3e3f8587146c767d2fa"
+      url "https://github.com/Intina47/jot/releases/download/v1.7.2/jot_v1.7.2_darwin_amd64.tar.gz"
+      sha256 "848cca6beb3a57e18053087729f22af55c3b21b3b8769b428b75f1f7d63345ae"
     end
   end
 
   on_linux do
-    url "https://github.com/Intina47/jot/releases/download/v1.7.0/jot_v1.7.0_linux_amd64.tar.gz"
-    sha256 "630185c8a9811d619545a2e47117c54f1529348ee0d7dacb290d8cf9793f7ff4"
+    url "https://github.com/Intina47/jot/releases/download/v1.7.2/jot_v1.7.2_linux_amd64.tar.gz"
+    sha256 "2678ba0645de9014ea99473a66b805635e90fcc09af5c7cded11526b266280f2"
   end
 
   def install
